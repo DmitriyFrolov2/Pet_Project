b447487f04a79ae94481b1fca395d89db02fc1dd
Dima <dmitrii.frolov2018@yandex.ru>
2024-08-27 23:22:38 +0300

    test

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 class BasePage:
     def __init__(self, driver, url):
         self.driver = driver
@@ -10,30 +9,27 @@ class BasePage:
     def open(self):
         self.driver.get(self.url)
 
+    def element_is_visible(self, locator, timeout=5):
+        try:
+            return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+        except Exception as e:
+            print(f"Element not found: {locator}")
+            raise e
 
-def element_is_visible(self, locator, timeout=5):
-    return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+    def element_are_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
-def element_are_visible(self, locator, timeout=5):
-    return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+    def element_are_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
 
+    def element_is_not_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
 
-def element_is_present(self, locator, timeout=5):
-    return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
-
-def element_are_present(self, locator, timeout=5):
-    return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
-
-
-def element_is_not_visible(self, locator, timeout=5):
-    return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
-
-
-def element_is_clickable(self, locator, timeout=5):
-    return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
-
-
-def go_to_element(self, element):
-    self.driver.execute_script("arguments[0].scrollIntoView();", element)
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
