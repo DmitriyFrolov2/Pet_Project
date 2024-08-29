4c7b345d3af0516454bf94e521f346a58af992be
Dima <dmitrii.frolov2018@yandex.ru>
2024-08-29 11:44:06 +0300

    коммит перед запуском

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # automation_qa_portfolio
+
+Python 3.12.2 or higher
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,5 +1,6 @@
 import time
 
+from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
@@ -7,11 +8,17 @@ class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys('324')
-        self.element_is_visible(self.locators.EMAIL).send_keys('233@imnox.ru')
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('4343')
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('p433ss')
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
+        return full_name, email, current_address, permanent_address
 
 
 
