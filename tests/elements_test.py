88e5b943d1bd369ddadab967828294201754eeb5
Dima <dmitrii.frolov2018@yandex.ru>
2024-09-03 17:33:12 +0300

    added test_RadioButton

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.elements_page import TextBoxPage, CheckBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage
 
 
 class TestElements:
@@ -25,5 +25,26 @@ class TestCheckBox:
         check_box_page.click_random_checkbox()
         input_checkbox = check_box_page.get_checked_checkboxes()
         output_result = check_box_page.get_output_result()
-        assert input_checkbox == output_result
+        assert input_checkbox == output_result, 'checkboxes have not been selected'
+
+class TestRadioButton:
+    def test_radio_button(self, driver):
+        radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
+        radio_button_page.open()
+        radio_button_page.click_radio_button('yes')
+        output_yes = radio_button_page.get_output_result()
+        radio_button_page.click_radio_button('impressive')
+        output_impressive = radio_button_page.get_output_result()
+        radio_button_page.click_radio_button('no')
+        output_no = radio_button_page.get_output_result()
+        assert output_yes == 'Yes'
+        assert output_impressive == 'Impressive'
+        assert output_no == 'No'
+
+
+
+
+
+
+
 
